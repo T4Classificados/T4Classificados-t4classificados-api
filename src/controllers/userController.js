eae4878a7103860699c84537a4e7ebe610d17d20
H3lio2f <heliofragaofila.helio@gmail.com>
2024-10-04 12:12:35 +0100

    updates

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -89,7 +89,7 @@ exports.loginUser = async (req, res) => {
     const accessToken = jwt.sign(
       { userId: user.id, telefone: user.telefone, role: user.role, nome: user.nome, sobrenome: user.sobrenome },
       process.env.JWT_SECRET,
-      { expiresIn: '20s' }
+      { expiresIn: '1h' }
     );
 
     const refreshToken = jwt.sign(
